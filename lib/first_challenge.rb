80b0b37529eb9ecc104e366872eb029f727b2927
Matt Pitner <mattpitner09@gmail.com>
2019-08-16 20:07:23 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,10 @@
 def first_challenge
    epic_tragedy = {
-     :montague =>{},
+     :montague =>{
+       :patriarch
+       :matriarch
+       :hero
+:hero_friends},
      :capulet => {}
    }
 end
